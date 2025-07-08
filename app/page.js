44dacb4ba71769f668258448407a7c602b03700e
Utkarsh Singh Rawat <utkarsh.srawat2003@gmail.com>
2025-07-08 15:10:29 +0530

    A ko add kiya hai main branch mai

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
+import A from "@/branch/a";
 import B from "@/branch/b";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      Main Branch <B/>
+      <A/> Main Branch <B/>
     </div>
   );
 }
